e1eb4a1d5e8e439e0d00f67ca959c5f282dfa1c4
penguinman6678 <not@setyet.com>
2020-02-08 16:52:52 -0800

    Adding a config file for a model for making predictions

---
+++ boardGames/model_config.py
@@ -0,0 +1,64 @@
+config_for_model_a = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-01-13-21-02-40_winAndLoss_Loss1KWeights_sample_focus_0.718_in_json.json",
+        model_h5 = "model_2020-01-13-21-02-40_winAndLoss_Loss1KWeights_sample_focus_0.718_weights.h5",
+        model_name="model_2020-01-17-23-24-05"
+    )
+config_for_model_b = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-01-17-23-44-03_winAndLoss_combinedWithUniq_sample_focus_0.703_in_json.json",
+        model_h5 = "model_2020-01-17-23-44-03_winAndLoss_combinedWithUniq_sample_focus_0.703_weights.h5",
+        model_name="model_2020-01-17-23-24-05"
+    )
+config_for_model_c = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-01-11-21-07-12_winAndLoss_sample_focus_0.75_in_json.json",
+        model_h5 = "model_2020-01-11-21-07-12_winAndLoss_sample_focus_0.75_weights.h5",
+        model_name="model_2020-01-17-23-24-05"
+    )
+
+config_for_model_d = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-01-22-23-13-23_winAndLoss_combinedWithUniq_sample_focus_MostDataAsTrain_in_json.json",
+        model_h5 = "model_2020-01-22-23-13-23_winAndLoss_combinedWithUniq_sample_focus_MostDataAsTrain_weights.h5",
+        model_name="model_2020-01-17-23-24-05"
+    )
+    ## BAD BAD...  the below model was trained only using win/loss foucsed data.
+config_for_model_e = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-01-25-00-22-48_winAndLoss_focused_data_only-0.66_in_json.json",
+        model_h5="model_2020-01-25-00-22-48_winAndLoss_focused_data_only-0.66_weights.h5",
+        model_name="model_from_onlyFocusedData"
+    )
+    # Conv2D model 1
+config_for_model_f = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-02-01-20-07-29_winAndLoss_all_with_conv2D-0.88_in_json.json",
+        model_h5="model_2020-02-01-20-07-29_winAndLoss_all_with_conv2D-0.88_weights.h5",
+        model_name="model_from_conv2_f"
+    )
+
+# Conv2D model 2
+config_for_model_g = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-02-02-19-08-30_winAndLoss_all_with_conv2D-0.66_in_json.json",
+        model_h5="model_2020-02-02-19-08-30_winAndLoss_all_with_conv2D-0.66_weights.h5",
+        model_name="model_from_conv2_g"
+    )
+
+# Conv2D model 3
+config_for_model_h = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-02-04-22-32-21_winAndLoss_all_with_conv2D-0.88_in_json.json",
+        model_h5="model_2020-02-04-22-32-21_winAndLoss_all_with_conv2D-0.88_weights.h5",
+        model_name="model_from_conv2_h"
+    )
+
+# Conv2D model 3
+config_for_model_i = dict(
+        model_dir="./train_model/models_ex/",
+        model_json="model_2020-02-08-11-14-06_winAndLoss_all_with_conv2D-0.61_in_json.json",
+        model_h5="model_2020-02-08-11-14-06_winAndLoss_all_with_conv2D-0.61_weights.h5",
+        model_name="model_from_config_conv2_i"
+    )
+
